26d27253c062046a5c700ed6e4658af4807c7ecc
Naresh Sekar <nareshnavinash@gmail.com>
2026-02-25 15:19:35 +0530

    chore: icon update

--- remotion/src/stills/MenuBarIconIdle.tsx
+++ remotion/src/stills/MenuBarIconIdle.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { AbsoluteFill } from "remotion";
 
-// Same paths as GhostLogo at 1024x1024 — SVG scales to 44px
+// Same paths as GhostLogo at 1024x1024 — viewBox padded so ghost height ≈ 15pt
 const GHOST_BODY_PATH =
   "M 512 175 C 654 175 742 280 742 430 L 742 660 C 742 720 720 760 684 760 C 648 760 630 700 598 700 C 566 700 548 760 512 760 C 476 760 458 700 426 700 C 394 700 376 760 340 760 C 304 760 282 720 282 660 L 282 430 C 282 280 370 175 512 175 Z";
 const MOUTH_PATH = "M 478 500 L 546 500 C 546 555 478 555 478 500 Z";
@@ -16,7 +16,7 @@ export const MenuBarIconIdle: React.FC = () => {
         backgroundColor: "transparent",
       }}
     >
-      <svg width={44} height={44} viewBox="197 152 630 630">
+      <svg width={44} height={44} viewBox="82 38 860 860">
         {/* Ghost body */}
         <path d={GHOST_BODY_PATH} fill="#FFFFFF" />
         {/* Eyes */}
